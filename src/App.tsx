1363299b3c903c4f79240eedbe7c527baf1f8e30
Lucile Lefebvre <lucile.c.lefebvre@gmail.com>
2024-05-20 13:00:24 +0200

    configure vercel for client-side routing

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,6 @@ import About from '@components/About.tsx'
 import Terms from '@components/Terms.tsx'
 import Home from '@components/Home.tsx'
 import Challenge from '@components/Challenge.tsx'
-// import Hero from '@components/Hero'
-// import Retreats from '@components/Retreats'
-// import Classes from '@components/Classes'
-// import Footer from '@components/Footer'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import "./App.css"
 
@@ -15,11 +11,6 @@ function App() {
 
   return (
     <Router>
-      {/* <nav>
-        <Link to="/">Home</Link>
-        <Link to="/Contact">About</Link>
-        <Link to="/contact">Contact</Link>
-      </nav> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -28,23 +19,7 @@ function App() {
       </Routes>
     </Router>
 
-    // <>
-    //   <div className='top-body'>
-    //     <Navbar/>
-    //     <Hero/>
-    //   </div>
-    //   <div className='bottom-body'>
-    //     <Retreats/>
 
-    //     <div className='grass-img'>
-    //       <p>LE YOGA A CHANGE MA VIE</p>
-    //       <p>UN JOUR A LA FOIS...</p>
-    //     </div>
-    //     <Classes/>
-
-    //     <Footer/>
-    //   </div>
-    // </>
   )
 }
 
